efea27cbc83aa3db870eec1f974a6bd74c7ea80d
Angel Torres <thekamitorres@gmail.com>
2015-10-27 02:21:59 -0600

    pequeña modificación

--- mini_reto.playground/Contents.swift
+++ mini_reto.playground/Contents.swift
@@ -1,29 +1,21 @@
-var numeros = 1...100
+var rango = 1...100
 
-for n in numeros{
+for n in rango{
     
     if n % 5 == 0{
-        
         print("\(n) => Bingo!!")
-        
     }
     
     if n % 2 == 0{
-        
         print("\(n) => Es Par!!")
-        
     }
     
     if n % 2 != 0{
-        
         print("\(n) => Es Impar!!")
-        
     }
     
     if n >= 30 && n <= 40 {
-        
         print("\(n) => Viva switft!!")
-        
     }
     
 }
